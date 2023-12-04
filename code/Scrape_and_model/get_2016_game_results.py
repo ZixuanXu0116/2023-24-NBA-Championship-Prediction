10e6df768a48ee772e998b170305c647f3b33ea7
AahilNav <aahilnavroz@gmail.com>
2023-12-03 20:11:28 -0600

    creating the new csv

--- code/Scrape_and_model/get_2016_game_results.py
+++ code/Scrape_and_model/get_2016_game_results.py
@@ -11,3 +11,23 @@ game_start_indices = demo_2016_df.index[demo_2016_df['Tm'].ne(demo_2016_df['Tm']
                                         demo_2016_df['date'].ne(demo_2016_df['date'].shift())].tolist()
 
 processed_games = []
+
+# Process each game
+for i in range(0, len(game_start_indices), 2):
+    if i + 1 < len(game_start_indices):  # Ensure there is a pair of indices
+        date = demo_2016_df.loc[game_start_indices[i], 'date']
+        team1 = demo_2016_df.loc[game_start_indices[i], 'Tm']
+        team2 = demo_2016_df.loc[game_start_indices[i + 1], 'Tm']
+        result = demo_2016_df.loc[game_start_indices[i + 1], 'Win']
+
+        processed_games.append({
+            'date': date,
+            'team1': team1,
+            'team2': team2,
+            'result': result
+        })
+# Create a DataFrame from the processed games
+final_df = pd.DataFrame(processed_games)
+
+# Save the final dataframe to a new CSV file
+final_df.to_csv('processed_demo_2016.csv', index=False)
