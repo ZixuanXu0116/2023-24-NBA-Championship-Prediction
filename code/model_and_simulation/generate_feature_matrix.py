d4d5d31003b7208f3604d7e13b1cb6a46de48a28
Zixuan Xu <143112661+ZixuanXu0116@users.noreply.github.com>
2023-12-08 22:20:44 -0600

    Update generate_feature_matrix.py

--- code/model_and_simulation/generate_feature_matrix.py
+++ code/model_and_simulation/generate_feature_matrix.py
@@ -1,24 +1,60 @@
 import pandas as pd
 from database import engine
 
-def get_feature_matrix(schedule_df, df):
 
-    df_sorted = df.groupby('Tm', group_keys=False).apply(lambda x: x.sort_values(by='MP', ascending=False))
+def get_feature_matrix(schedule_df, df):
+    df_sorted = df.groupby('Tm', group_keys=False).apply(
+        lambda x: x.sort_values(by='MP', ascending=False)
+    )
     df_sorted = df_sorted[df_sorted['Tm'] != 'TOT']
 
     '''Selecting the top 9 players based on average minutes played'''
 
     players_df = df_sorted.groupby('Tm').head(9)
 
-
-    result_df = pd.concat([schedule_df.apply(lambda x: players_df.loc[players_df['Tm'] == x['team1'], 
-                        ['Pos', 'MP', 'Age', 'shooting', 'peri_def', 'playmaker', 
-                        'pro_rim', 'efficiency', 'influence', 'scoring']].values.flatten(), axis=1, result_type='expand'),
-
-                        schedule_df.apply(lambda x: players_df.loc[players_df['Tm'] == x['team2'], 
-                        ['Pos', 'MP', 'Age', 'shooting', 'peri_def', 'playmaker', 
-                        'pro_rim', 'efficiency', 'influence', 'scoring']].values.flatten(), axis=1, result_type='expand')],
-                        axis=1)
+    result_df = pd.concat(
+        [
+            schedule_df.apply(
+                lambda x: players_df.loc[
+                    players_df['Tm'] == x['team1'],
+                    [
+                        'Pos',
+                        'MP',
+                        'Age',
+                        'shooting',
+                        'peri_def',
+                        'playmaker',
+                        'pro_rim',
+                        'efficiency',
+                        'influence',
+                        'scoring',
+                    ],
+                ].values.flatten(),
+                axis=1,
+                result_type='expand',
+            ),
+            schedule_df.apply(
+                lambda x: players_df.loc[
+                    players_df['Tm'] == x['team2'],
+                    [
+                        'Pos',
+                        'MP',
+                        'Age',
+                        'shooting',
+                        'peri_def',
+                        'playmaker',
+                        'pro_rim',
+                        'efficiency',
+                        'influence',
+                        'scoring',
+                    ],
+                ].values.flatten(),
+                axis=1,
+                result_type='expand',
+            ),
+        ],
+        axis=1,
+    )
 
     query = f'SELECT * FROM real_total_feature_matrix_data WHERE season = 2023'
     use_name_df = pd.read_sql_query(query, engine)
