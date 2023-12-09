54cba64f20b6434b39e7e46f1d118c68b35407ca
Zixuan Xu <143112661+ZixuanXu0116@users.noreply.github.com>
2023-12-08 22:27:01 -0600

    Update get_predicted_total_feature_matrix.py

--- code/model_and_simulation/get_predicted_total_feature_matrix.py
+++ code/model_and_simulation/get_predicted_total_feature_matrix.py
@@ -4,7 +4,7 @@ import ast
 from database import engine
 from pandas import json_normalize
 
-new_directory = os.path.join(os.getcwd(), "code", "model_and_simulation")
+new_directory = os.path.join(os.getcwd(), 'code', 'model_and_simulation')
 os.chdir(new_directory)
 df = pd.read_csv(os.path.join(os.getcwd(), 'temp_predicted.csv'))
 
@@ -25,11 +25,12 @@ df_team2_matrix = json_normalize(df['team2'].apply(lambda x: x['matrix']))
 
 '''Concatenate the flattened features and matrix'''
 
-df = pd.concat([df_team1_features, df_team1_matrix, df_team2_features, df_team2_matrix], axis=1)
+df = pd.concat(
+    [df_team1_features, df_team1_matrix, df_team2_features, df_team2_matrix], axis=1
+)
 
 dict_columns = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 for col in dict_columns:
-
     list_temp1 = df[col].iloc[:, 0].values.tolist()
     temp1 = pd.DataFrame(list_temp1)
 
@@ -39,17 +40,67 @@ for col in dict_columns:
     index1 = str((col + 1) * 10 + 1)
     index2 = str((col + 1) * 10 + 2)
 
-    df[[f'Pos{index1}', f'MP{index1}', f'Age{index1}', f'shooting{index1}', f'peri_def{index1}', \
-        f'playmaker{index1}', f'pro_rim{index1}', f'efficiency{index1}', f'influence{index1}',f'scoring{index1}']] = \
-    temp1[['Pos', 'MP', 'Age', 'shooting', 'peri_def', 'playmaker', 'pro_rim', 'efficiency', 'influence', 'scoring']]
+    df[
+        [
+            f'Pos{index1}',
+            f'MP{index1}',
+            f'Age{index1}',
+            f'shooting{index1}',
+            f'peri_def{index1}',
+            f'playmaker{index1}',
+            f'pro_rim{index1}',
+            f'efficiency{index1}',
+            f'influence{index1}',
+            f'scoring{index1}',
+        ]
+    ] = temp1[
+        [
+            'Pos',
+            'MP',
+            'Age',
+            'shooting',
+            'peri_def',
+            'playmaker',
+            'pro_rim',
+            'efficiency',
+            'influence',
+            'scoring',
+        ]
+    ]
 
-    df[[f'Pos{index2}', f'MP{index2}', f'Age{index2}', f'shooting{index2}', f'peri_def{index2}', \
-        f'playmaker{index2}', f'pro_rim{index2}', f'efficiency{index2}', f'influence{index2}',f'scoring{index2}' ]] = \
-    temp2[['Pos', 'MP', 'Age', 'shooting', 'peri_def', 'playmaker', 'pro_rim', 'efficiency', 'influence','scoring']]
+    df[
+        [
+            f'Pos{index2}',
+            f'MP{index2}',
+            f'Age{index2}',
+            f'shooting{index2}',
+            f'peri_def{index2}',
+            f'playmaker{index2}',
+            f'pro_rim{index2}',
+            f'efficiency{index2}',
+            f'influence{index2}',
+            f'scoring{index2}',
+        ]
+    ] = temp2[
+        [
+            'Pos',
+            'MP',
+            'Age',
+            'shooting',
+            'peri_def',
+            'playmaker',
+            'pro_rim',
+            'efficiency',
+            'influence',
+            'scoring',
+        ]
+    ]
 
-df = df.drop(columns = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14])
+df = df.drop(columns=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
 df['season'] = season
 df['date'] = date
 df['result'] = y
 
-df.to_sql('predicted_total_feature_matrix_data', con=engine, if_exists='replace', index=False)
+df.to_sql(
+    'predicted_total_feature_matrix_data', con=engine, if_exists='replace', index=False
+)
